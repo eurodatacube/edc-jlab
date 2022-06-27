fdeb154900e2d6b49d96eb9f93f7eba212a76ae7
Bernhard Mallinger <bernhard.mallinger@eox.at>
2022-06-27 12:41:56 +0200

    Remove old contribution code (not compatible with current jupyter_server)
    API calls to hidden files are now forbidden.

--- src/contribute.ts
+++ src/contribute.ts
@@ -11,40 +11,6 @@ import { toArray } from "@lumino/algorithm";
 import { NOTEBOOK_ICON_CLASS } from "./constants";
 
 
-export const CONTRIBUTE_STAGING_PATH = ".contribute-staging";
-
-
-async function ensureStatingDirExists(
-  contents: Contents.IManager
-): Promise<void> {
-  // We should check if the dir exists, but we the server doesn't allow accessing
-  // hidden files. However we can create it and check if the response is 409 conflict.
-  // Also, the API doesn't allow creating dirs, only untitled dirs which are later
-  // renamed.
-  console.log(`Trying to create ${CONTRIBUTE_STAGING_PATH}`);
-  const dir = await contents.newUntitled({ type: "directory" });
-  try {
-    try {
-      await contents.rename(dir.name, CONTRIBUTE_STAGING_PATH);
-    } catch (error) {
-      if (error.response.status == 404) {
-        // this means success, lol. probably the server filters hidden dirs, so
-        // it doesn't find the dir it has renamed.
-        console.log("Created dir successfully");
-      } else {
-        throw error;
-      }
-    }
-  } catch (error) {
-    if (error.response.status == 409) {
-      // CONFLICT
-      await contents.delete(dir.name);
-      console.log("Directory already exists");
-    } else {
-      throw error;
-    }
-  }
-}
 /**
  * Add a context menu entry which triggers contributions
  */
@@ -80,27 +46,13 @@ export function activateContribute(
         items.map((item) => item.path).join(", ")
       );
       if (items.length > 0) {
-        const copyPromises = items.map(async (item: Contents.IModel) => {
-          // make sure to delete target first in case it's an update
-          try {
-            await docmanager.deleteFile(
-              `${CONTRIBUTE_STAGING_PATH}/${item.name}`
-            );
-          } catch (error) {
-            // 404 error is expected, everything else is bad
-            if (error.response.status !== 404) {
-              throw error;
-            }
-          }
-
-          return docmanager.copy(item.path, CONTRIBUTE_STAGING_PATH);
-        });
+        // TODO: make call to new backend endpoint (TODO) to copy the files to .contribute-staging
+        const contributePromise = do_stuff();
 
         // need to open window right now in click handler, it's not allowed
         // in promise handler.
         const newTab = window.open("", "_blank");
-        Promise.all(copyPromises)
-          .then(() => {
+        contributePromise.then(() => {
             // TODO: open according contribute page (dev/prod)
             // NOTE: market place is currently restricted to 1 contributed file
             newTab.location.href = `https://eurodatacube.com/contributions/jupyter-notebook/new/${items[0].name}`;
@@ -121,6 +73,4 @@ export function activateContribute(
     command: contributeCommandId,
     rank: 9, // about at the end of file-operations
   });
-
-  ensureStatingDirExists(docmanager.services.contents);
 }
